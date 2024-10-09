9b7cb2f52fa8e7652526609584f599a7ba95766e
NhatHM4 <haminhnhat711@gmail.com>
2024-10-09 21:40:48 +0700

    video #100

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -45,7 +45,8 @@ public class SecurityConfiguration {
                 .csrf(c -> c.disable())
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(
-                        (request) -> request.requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh")
+                        (request) -> request
+                                .requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh", "/storage/**")
                                 .permitAll()
                                 .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
